28b11c7ee613b97233a825c2ddcfc8f58526fb58
Naiver <sketchyandersen@gmail.com>
2023-10-30 14:34:58 +0100

    Start Screen - Astrid
    Start screen setup

---
+++ Assets/StartScreen/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Shifts the scene to the Game scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        // Just to quits the game
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
