f876c59a4911eff9a8a05b33a260b220dc52178c
Emil Madsen <Nesstark0@gmail.com>
2023-11-01 09:40:07 +0100

    Update TetrisBlock.cs
    Collider

--- Assets/Scripts/TetrisBlock.cs
+++ Assets/Scripts/TetrisBlock.cs
@@ -78,6 +78,9 @@ public class TetrisBlock : MonoBehaviour
             {
                 return false;
             }
+
+            if (grid[roundedX, roundedY] != null)
+                return false;
         }
 
         return true;
