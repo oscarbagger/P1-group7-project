70c696a10bb1bfd59c515c76ddeac5ea94e3418e
oscarbagger <oscarbagger@live.dk>
2023-11-01 12:04:50 +0100

    Update Spawn.cs

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -6,18 +6,37 @@ public class Spawn : MonoBehaviour
 {
     public GameObject[] Tetrominoes;
     // Start is called before the first frame update
+    public List<int> blocksToSpawn = new List<int>();
+
     void Start()
     {
+        for(int i=0;i<3; i++)
+        {
+            AddBlockToList();        
+        }
         NewTetromino();
     }
 
     public void NewTetromino()
     {
-        Instantiate(Tetrominoes[Random.Range(0, Tetrominoes.Length)], transform.position, Quaternion.identity);
+        Instantiate(Tetrominoes[blocksToSpawn[0]], transform.position, Quaternion.identity);
+        blocksToSpawn.RemoveAt(0);
+        AddBlockToList();
+        Debug.Log("Next block is: "+Tetrominoes[blocksToSpawn[0]]);
     }
-    // Update is called once per frame
-    void Update()
+
+    public void AddBlockToList()
     {
-       
+        int randomInt= Random.Range(0, Tetrominoes.Length);
+        if (blocksToSpawn.Count>2)
+        {
+            // if randomInt is the same as last 2 numbers in list, then reroll it till its not the same. 
+            while (randomInt==blocksToSpawn[blocksToSpawn.Count-1] && randomInt==blocksToSpawn[blocksToSpawn.Count-2])
+            {
+                randomInt = Random.Range(0, Tetrominoes.Length);
+            }
+        }
+        blocksToSpawn.Add(randomInt);
+
     }
 }
