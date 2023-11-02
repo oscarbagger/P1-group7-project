a8bec91f7e2eee2164f5bfed4a8288437a04ff5d
Emil Madsen <Nesstark0@gmail.com>
2023-11-02 14:19:50 +0100

    Commit
    Nyt script til player controller, virker ikke endnu, så alle funktioner er stadig inde i scriptet "TetrisBlock" (Skal have nyt navn at some point)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+/* Kode virker ikke endnu, men er gemt her for nu
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private TetrisBlock tetrisBlock; // Reference to the TetrisBlock script
+
+    private void Update()
+    {
+        if (tetrisBlock == null)
+        {
+            // Find the TetrisBlock script if it's not set
+            tetrisBlock = FindObjectOfType<TetrisBlock>();
+        }
+
+        if (tetrisBlock == null)
+        {
+            Debug.LogError("TetrisBlock script not found.");
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            tetrisBlock.MoveLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            tetrisBlock.MoveRight();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            tetrisBlock.HardDrop();
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            tetrisBlock.Rotate();
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tetrisBlock.MoveDown();
+        }
+    }
+}
+
+*/ 
