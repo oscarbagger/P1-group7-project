2007225314f6fd956640a34953083add728334e2
KunstnerDrengen <89451801+KunstnerDrengen@users.noreply.github.com>
2023-11-03 10:32:52 +0100

    Game Over
    Virker nok måske hvem ved

---
+++ Assets/Scripts/GAME_OVER.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GAME_OVER : MonoBehaviour
+{
+    /*
+    private bool GameEnd = false;
+    private bool GameOverScene = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameEnd)
+        {
+            SceneManager.LoadScene("Game over");
+        }
+    }
+
+    
+    //Game over funktion//
+    void GameOver()
+    {
+        for (int i = height - 1; i >= 0; i--)
+        {
+            if (GameEnd)
+            {
+                GameOverScene = true;
+            }
+        }
+
+    }
+
+    void Row18Hit(int i)
+    {
+        for (int j = 0; j < width; j++)
+            if (grid[height - 1, j] != null)
+            {
+                GameEnd = true;
+            }
+    }
+    */
+}
