21ee7490b31787d206f0e0adea755ad7ca8fe665
oscarbagger <oscarbagger@live.dk>
2023-11-06 12:19:49 +0100

    Updated blocks rotation points

--- Assets/Scripts/TextHolder.cs
+++ Assets/Scripts/TextHolder.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class TextHolder : MonoBehaviour
 {
-    private int textIndex=-1;
+    private int textIndex=-1; // index of text to be displayed on next block. 
     [Multiline]
-    public string[] negativeBlockText;
+    public string[] negativeBlockText; // array of text to put on blocks. Written in inspector.
 
+    // returns the next text to be displayed on a block. as long as there are more texts in the array keep picking the next one.
     public string GetNewText()
     {
         if (textIndex< negativeBlockText.Length)
--- Assets/Scripts/TextOnBlock.cs
+++ Assets/Scripts/TextOnBlock.cs
@@ -10,10 +10,11 @@ public class TextOnBlock : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        myText = GetComponentInChildren<TextMeshPro>();
-        textList = GameObject.Find("TextManager").GetComponent<TextHolder>();
-        SetBlockText(textList.GetNewText());
+        myText = GetComponentInChildren<TextMeshPro>(); // get textmesh component from child gameobject
+        textList = GameObject.Find("TextManager").GetComponent<TextHolder>(); // get reference to textholder
+        SetBlockText(textList.GetNewText()); // get text from textholders list of texts
     }
+    // set new text on block
     public void SetBlockText(string text)
     {
         myText.text = text;
