61b39b14e2cbf7bfaf2bd484d8f702cc2e1bde91
oscarbagger <oscarbagger@live.dk>
2023-12-15 10:50:47 +0100

    text writing in disclaimer

--- Assets/Scripts/Menus/TextWrite.cs
+++ Assets/Scripts/Menus/TextWrite.cs
@@ -6,22 +6,23 @@ using TMPro;
 public class TextWrite : MonoBehaviour
 {
     [SerializeField] private TMP_Text textToWrite;
-    [SerializeField] private float timePerCharacter;
+    [SerializeField] private float timePerCharacter; // time between each new text character being shown
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(WriteText());
+        StartCoroutine(WriteText()); // start writing out text
     }
     private IEnumerator WriteText()
     {
-        int textLength = textToWrite.text.Length;
-        string myText = textToWrite.text;
-        textToWrite.text = myText.Insert(0, "<alpha=#00>");
+        int textLength = textToWrite.text.Length; // get length of chosen text
+        string myText = textToWrite.text; // make a copy of the text to manipulate for later
+        textToWrite.text = myText.Insert(0, "<alpha=#00>"); // set text invisible by inserting <alpha> attribute into start of the text
         int i = 0;
+        // insert the <alpha> attribute to a new place each time, revealing a new character of the text, until everything is visible.
         while (i<textLength)
         {
-            textToWrite.text=myText.Insert(i, "<alpha=#00>");
-            yield return new WaitForSeconds(timePerCharacter);
+            textToWrite.text=myText.Insert(i, "<alpha=#00>"); 
+            yield return new WaitForSeconds(timePerCharacter); // delay
             i++;
         }
 
